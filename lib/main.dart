590399e6e606d60227ae52f6c48063fac6182554
root <root@DESKTOP-2TH44RN.localdomain>
2020-12-30 11:47:18 +0900

    add first test

---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'reg.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(AuthExampleApp());
+}
+
+/// The entry point of the application.
+///
+/// Returns a [MaterialApp].
+class AuthExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Link_Login',
+        home: Scaffold(
+          body: AuthTypeSelector(),
+        ));
+  }
+}
+
+/// Provides a UI to select a authentication type page
+class AuthTypeSelector extends StatelessWidget {
+  // Navigates to a new page
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context) /*!*/ .push(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Link_Login"),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+            return FlatButton(
+              child: FirebaseAuth.instance.currentUser != null
+                  ? const Text('Sign out')
+                  : const Text('Please Login'),
+              textColor: Theme.of(context).buttonColor,
+              onPressed: () async {
+                final User user = await _auth.currentUser;
+                if (user == null) {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('No one has signed in.'),
+                  ));
+                  return;
+                }
+                _signOut();
+                final String uid = user.uid;
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(uid + ' has successfully signed out.'),
+                ));
+              },
+            );
+          })
+        ],
+      ),
+      body: Column(
+        children: [_EmailPasswordForm()],
+      ),
+    );
+  }
+
+  void _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signOut();
+    // user = null;
+  }
+}
+
+class _EmailPasswordForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _EmailPasswordFormState();
+}
+
+class _EmailPasswordFormState extends State<_EmailPasswordForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: const Text(
+                    '구글로 로그인',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  alignment: Alignment.center,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  alignment: Alignment.center,
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: "Sign In",
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        //_signInWithEmailAndPassword();
+                        _signInWithGoogle();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // Example code of how to sign in with email and password.
+  /*void _signInWithEmailAndPassword() async {
+    try {
+      final User user = (await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ))
+          .user;
+
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("${user.email} signed in"),
+      ));
+    } catch (e) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to sign in with Email & Password"),
+      ));
+    }
+  }*/
+
+  void _signInWithGoogle() async {
+    try {
+      UserCredential userCredential;
+
+      if (kIsWeb) {
+        GoogleAuthProvider googleProvider = GoogleAuthProvider();
+        userCredential = await _auth.signInWithPopup(googleProvider);
+      } else {
+        final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        final GoogleAuthCredential googleAuthCredential =
+            GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        userCredential = await _auth.signInWithCredential(googleAuthCredential);
+      }
+
+      final user = userCredential.user;
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Sign In ${user.uid} with Google"),
+      ));
+      Navigator.push(context, MaterialPageRoute(builder: (_) {
+        return Grade();
+      }));
+    } catch (e) {
+      print(e);
+
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to sign in with Google: ${e}"),
+      ));
+    }
+  }
+}
